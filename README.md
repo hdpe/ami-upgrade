7e5f59c0898a6a25bd6b7203a2a3ed3fe4adfef2
Ryan Pickett <ryan@hdpe.me>
2018-02-05 23:56:30 +0000

    Misc. fixes and improvements

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AMI Upgrade
 
-Start a pre-existing EC2 "template" instance, `yum` update everything, stop, kill any existing AMIs and snapshots from the old template, and register a new AMI from the updated instance.
+Start a pre-existing EC2 "template" instance, run a user script on it, stop, kill any existing AMIs and snapshots from the old template, and register a new AMI from the updated instance.
 
 Handy for transient instances like Jenkins slaves where you don't want to be manually regenerating the AMI all the time, and you don't want to delay the instance's startup by sticking lots of yum updating in instance user data.
 
@@ -8,24 +8,43 @@ Handy for transient instances like Jenkins slaves where you don't want to be man
 
 ### Environment
 
-`.env`:
+`.env`
 
 ```
-AWS_ACCESS_KEY=blahblahblahkey
+# AWS credentials
+AWS_ACCESS_KEY_ID=blahblahblahkey
 AWS_SECRET_ACCESS_KEY=blahblahblahblahblahblahblahblahblahblahkey
 AWS_DEFAULT_REGION=eu-west-2
-IDENTITY_FILE=/.key
+
+# Value of the "Name" tag for the template instance
 INSTANCE_NAME_TAG=My Jenkins Slave Template Instance
+
+# Name of the image to be destroyed and created
 AMI_NAME=AmazonLinuxNodeDockerJenkinsSlave
+
+# IP to use: Private (default) or Public
+IP_TYPE=Private
+
+# User to use for SSH
 REMOTE_USER=ec2-user
+
+# Any extra SSH args (you get -q for free)
+SSH_ARGS=-i /.id_rsa -tt -o StrictHostKeyChecking=no
+
+# The command to run on the remote!
+USER_SCRIPT=sudo yum update -y
+
+# Output everything (false - default), or just the new image ID (true)
+QUIET=false
 ```
 
 ### Run script
 
 ```
-$ docker run --rm --env-file .env -v ~/.ssh/id_rsa:/.key ami-upgrade
+$ docker run --rm --env-file .env -v ~/.ssh/id_rsa:/.id_rsa ami-upgrade
 ```
 
 ## TODO
 
-What you using `jq` for? Use aws-cli's native JSON wrangling.
+* What you using `jq` for? Use aws-cli's native JMESPath JSON wrangling
+* How can CloudFormation help with all this?
--- ami-upgrade.sh
+++ ami-upgrade.sh
@@ -2,56 +2,93 @@
 set -e
 
 # mandatory env vars for AWS client
-: ${AWS_ACCESS_KEY:?}
+: ${AWS_ACCESS_KEY_ID:?}
 : ${AWS_SECRET_ACCESS_KEY:?}
 : ${AWS_DEFAULT_REGION:?}
 
 # mandatory env vars for script
 : ${AMI_NAME:?}
 : ${INSTANCE_NAME_TAG:?}
-: ${IDENTITY_FILE:?}
 : ${REMOTE_USER:?}
+: ${USER_SCRIPT:?}
+
+# optional env vars for script
+: ${IP_TYPE:="Private"}
+: ${SSH_ARGS:=""}
+: ${QUIET:="false"}
+
+log() {
+    if [ "$QUIET" = "false" ]; then
+	echo $@
+    fi
+}
+
+log "determining instance ID for $INSTANCE_NAME_TAG"
 
 INSTANCE_ID=$(aws ec2 describe-instances \
     --filters "Name=tag:Name,Values=$INSTANCE_NAME_TAG" \
         | jq -r '.Reservations[0].Instances[0].InstanceId') 
 
-aws ec2 start-instances --instance-ids $INSTANCE_ID
+log "starting ${INSTANCE_ID:?}"
+
+aws ec2 start-instances --instance-ids $INSTANCE_ID > /dev/null
+
+log "waiting for $INSTANCE_ID to start"
 
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
-HOST=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-        | jq -r '.Reservations[0].Instances[0].PrivateIpAddress') 
+log "determining IP ($IP_TYPE) for $INSTANCE_ID"
 
-until nc -z $HOST 22; do 
-    echo 'waiting for SSH...'
+HOST=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+        | jq -r ".Reservations[0].Instances[0].${IP_TYPE}IpAddress") 
+
+until nc -z ${HOST:?} 22; do 
+    log 'waiting for SSH...'
     sleep 1
 done
 
-mkdir -p ~/.ssh && ssh-keyscan -H -t rsa $HOST > ~/.ssh/known_hosts
+log "executing user script"
 
-ssh -i "$IDENTITY_FILE" -tt $REMOTE_USER@$HOST 'sudo yum update -y'
+ssh -q $SSH_ARGS $REMOTE_USER@$HOST "$USER_SCRIPT" > /dev/null
 
-aws ec2 stop-instances --instance-ids $INSTANCE_ID
+log "stopping $INSTANCE_ID"
+
+aws ec2 stop-instances --instance-ids $INSTANCE_ID > /dev/null
+
+log "waiting for $INSTANCE_ID to stop"
 
 aws ec2 wait instance-stopped --instance-ids $INSTANCE_ID
+
+log "determining previous image ID for $AMI_NAME"
 
 CURRENT_IMAGE_ID=$(aws ec2 describe-images --filters "Name=name,Values=$AMI_NAME" \
     | jq -r '.Images[0].ImageId // empty')
 
 if [ -n "$CURRENT_IMAGE_ID" ]; then
+
+    log "getting snapshot IDs for $CURRENT_IMAGE_ID"
+	
     SNAPSHOT_IDS=$(aws ec2 describe-images --image-ids $CURRENT_IMAGE_ID \
 	| jq -r '.Images[].BlockDeviceMappings[].Ebs.SnapshotId')
+
+    log "deregistering image $CURRENT_IMAGE_ID"
 
     aws ec2 deregister-image --image-id $CURRENT_IMAGE_ID
 
     for SNAPSHOT_ID in ${SNAPSHOT_IDS}; do
+
+	log "deleting snapshot $SNAPSHOT_ID"
+
 	aws ec2 delete-snapshot --snapshot-id $SNAPSHOT_ID
     done
 fi
 
+log "creating new image from $INSTANCE_ID"
+
 NEW_IMAGE_ID=$(aws ec2 create-image --instance-id $INSTANCE_ID --name "$AMI_NAME" \
     | jq -r '.ImageId')
+
+log "waiting for image ${NEW_IMAGE_ID:?} to become available"
 
 aws ec2 wait image-available --image-ids $NEW_IMAGE_ID
 
